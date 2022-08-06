697b1198cad34ad3feee8177927393a72b5dbe8f
Merdasa-Belay <merdasabelay@gmail.com>
2022-08-06 12:05:27 +0300

    [Feature added]

--- shell.h
+++ shell.h
@@ -27,5 +27,34 @@ int handle_builtin(char **command, char *line);
 void exit_cmd(char **command, char *line);
 
 void print_env(void);
+/* string handlers */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void execution(char *cp, char **cmd);
+char *find_path(void);
+
+/* helper function for efficient free */
+void free_buffers(char **buf);
+
+struct builtin
+{
+char *env;
+char *exit;
+} builtin;
+
+struct info
+{
+int final_exit;
+int ln_count;
+} info;
+
+struct flags
+{
+bool interactive;
+} flags;
 
 #endif /* SHELL_H */
