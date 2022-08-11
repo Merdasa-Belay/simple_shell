af3aef09038103ef9dd0836b55b741b6bb4a5254
Merdasa-Belay <merdasabelay@gmail.com>
2022-08-11 04:10:08 +0300

    [Added feature]

--- aux_error2.c
+++ aux_error2.c
@@ -68,3 +68,18 @@ char *error_path_126(data_shell *datash)
 	free(ver_str);
 	return (error);
 }
+
+/**
+ * aux_help_exit - Help information fot the builint exit
+ * Return: no return
+ */
+void aux_help_exit(void)
+{
+	char *help = "exit: exit [n]\n Exit shell.\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Exits the shell with a status of N. If N is ommited, the exit";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "statusis that of the last command executed\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
